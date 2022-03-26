128b05246b0a53ae3abec03ae1c0441120fac2cd
toaha.bhuiyan <toahabhuiyan@gmail.com>
2022-03-27 00:26:41 +0600

    added authorization on other goal apis

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,8 +1,9 @@
 const asyncHandler = require("express-async-handler")
 const Goal = require("./../model/goalModel");
+const User = require("../model/userModel");
 
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find();
+    const goals = await Goal.find({user: req.user.id});
     res.status(200).json(goals);
 })
 
@@ -14,6 +15,7 @@ const setGoal = asyncHandler( async (req, res) => {
 
     const goal = await Goal.create({
         text: req.body.text,
+        user: req.user.id
     })
 
     res.status(201).json(goal);
@@ -25,6 +27,18 @@ const updateGoal = asyncHandler( async (req, res) => {
     if(!goal) {
         res.status(400);
         throw new Error("Goal not found");
+    }
+
+    const user = await User.findById(req.user.id);
+
+    if(!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+
+    if(goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error("User not authorized");
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
@@ -39,6 +53,19 @@ const deleteGoal = asyncHandler( async (req, res) => {
         res.status(400);
         throw new Error("Goal not found");
     }
+
+    const user = await User.findById(req.user.id);
+
+    if(!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+
+    if(goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error("User not authorized");
+    }
+
 
     await goal.remove();
 
--- backend/routes/goalRouts.js
+++ backend/routes/goalRouts.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const {getGoals, setGoal, updateGoal, deleteGoal} = require("./../controller/goalController");
+const {protect} = require("./../middleware/authMiddleware");
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").delete(deleteGoal).put(updateGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal);
 
 // router.get("/", getGoals);
 
